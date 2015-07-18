b22cbedfb7dc728906226512ff7baf8f5850e353
Sean Voeller <sean.voeller@gmail.com>
2015-07-18 09:16:27 -0700

    converting to testng, adding liquibase plugin

---
+++ src/test/java/com/seanvoeller/AppTest.java
@@ -0,0 +1,11 @@
+package com.seanvoeller;
+
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue( true );
+    }
+}
